ad921deed9ceff3b5edf252bc985b841f651221c
Christopher Lazar <chrislazar3@aol.com>
2018-10-20 21:15:03 -0700

    Updated Exection script comments.

--- local-mode/ExecChefClientLocally.sh
+++ local-mode/ExecChefClientLocally.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-berks vendor cookbooks # Use the berks file in this directory to bring down the cookbooks into a subfolder named cookbooks, which holds the vendored cookbooks. Run each time a change is made in the cookbook.
-chef-client -z -o gamingpc # Exec the Chef Client locally.
+berks vendor cookbooks # Use the berks file in this directory to bring down the cookbooks into a subfolder named cookbooks, which holds the vendored cookbooks. Run each time a change is made in the gamingpc cookbook.
+chef-client -z -o gamingpc # Exec the Chef Client locally, against the gamingpc cookbook.
