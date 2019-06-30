201323c039d8b72ceec6355e6a9fab7508301e88
Xypherius <chrislazar3@aol.com>
2019-06-29 20:36:48 -0700

    Modified Initial configuration script.

--- Configure-GamingPC.ps1
+++ Configure-GamingPC.ps1
@@ -1,3 +1,5 @@
+# Run the below script to install Chocolatey, ChefDK, and Git. The script will then call into Chef Client, to configure the gaming PC and install the DSC and required gaming packages. #
+
 # Installs Chocolatey if it doesn't exist
 if (!(Test-Path 'C:\\ProgramData\\chocolatey')){
     Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
@@ -6,16 +8,21 @@ else{
     Write-Host 'Chocolatey alaready exists!!! Moving on...' -Foregroundcolor Yellow
 }
 
-refreshenv
-
-# Install ChefDK 
-choco install chefdk -y
-# Install Git CLI
-choco install Git -y
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
 
 refreshenv
 
-$ChefWorkspace = '/workspace\Chef'
+# Install ChefDK (Using force to install every time.)
+choco install chefdk -y -force
+# Install Git CLI (Using force to install every time.)
+choco install Git -y -force
+
+refreshenv
+
+$ChefWorkspace = '/Workspace\Chef'
 
 # Configure Git Workspace and Execute Chef Client, which will then configure the gaming PC.
 if (!(Test-Path $ChefWorkspace)){
@@ -27,14 +34,14 @@ else{
 
 # Clone down the Git Repo which holds the gaming pc cookbook, it doesn't already exist:
 if (!(Test-Path "$($ChefWorkspace)\cookbook-gamingpc")){
+    cd $ChefWorkspace
     git clone https://github.com/Xypherius/cookbook-gamingpc.git
-
 }
 else{
     Write-Host 'Coding Workspace already exists!! Moving on...' -Foregroundcolor Yellow
 }
 
-
 # Configuring Gaming PC
-cd C:\Workspace\Chef\cookbook-gamingpc\exec-chef-locally
-.\Execute-Cookbook-Run.ps1
+cd "$($ChefWorkspace)\cookbook-gamingpc\exec-chef-locally"
+berks vendor cookbooks
+chef-client -z -o gamingpc
