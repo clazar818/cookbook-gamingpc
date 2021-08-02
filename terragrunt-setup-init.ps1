5841e460f88f4bf8400ff6d51f45d0e968263170
Xypherius <44285823+Xypherius@users.noreply.github.com>
2021-08-02 15:57:39 -0700

    Create terragrunt-setup-init.ps1

---
+++ terragrunt-setup-init.ps1
@@ -0,0 +1,4 @@
+# exec in ps1 terminal window
+$env:TF_DATA_DIR = ".\.tf.out\"
+$env:TERRAGRUNT_DOWNLOAD = ".\.tg.out\"
+# https://syntaxbytetutorials.com/add-a-github-ssh-key-on-windows/ SSH Key needed
